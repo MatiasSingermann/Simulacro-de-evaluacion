9492b7ca1bb3e510b18c0e0104365c90e7cc1cfa
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-04-07 15:04:49 -0300

    Ejercicios 1 y 2 terminados

--- Assets/Scripts/Ejercicio1.cs
+++ Assets/Scripts/Ejercicio1.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Ejercicio1 : MonoBehaviour
 {
+    // 1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+    public int numero1;
+    public int numero2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (numero1 == numero2)
+        {
+            Debug.Log("Los valores son iguales");
+        }
+
+        else
+        {
+            Debug.Log("Los valores son distintos");
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Ejercicio2.cs
+++ Assets/Scripts/Ejercicio2.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Ejercicio2 : MonoBehaviour
 {
+    // 2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y
+    // un monto de dinero disponible. Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+    // Luego indicar cuánto dinero sobra o falta.
+
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    float preciototal;
+    public float monto;
+    float sobra;
+    float falta;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        preciototal = precio1 + precio2 + precio3;
+
+        if (monto >= preciototal)
+        {
+            Debug.Log("La suma de los precios no supera el monto disponible");
+            sobra = monto - preciototal;
+            Debug.Log("Sobran $" + sobra);
+        }
+
+        else
+        {
+            Debug.Log("La suma de los precios supera el monto disponible");
+            falta = preciototal - monto;
+            Debug.Log("Faltan $" + falta);
+        }
     }
 
     // Update is called once per frame
